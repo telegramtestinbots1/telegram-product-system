43d31544359c9931511d0ecd55d4eec65833c7c2
telegramtestinbots1 <yarenalaca1221775@gmail.com>
2026-04-19 02:13:35 +0300

    Add files via upload

---
+++ server.js
@@ -0,0 +1,84 @@
+const express = require("express");
+const fs = require("fs");
+
+const app = express();
+
+app.use(express.json());
+
+// Ürün oku
+
+app.get("/product", (req, res) => {
+
+    const data = fs.readFileSync("product.json");
+
+    res.json(JSON.parse(data));
+
+});
+
+// Ürün güncelle
+
+app.post("/update-product", (req, res) => {
+
+    fs.writeFileSync(
+        "product.json",
+        JSON.stringify(req.body, null, 2)
+    );
+
+    res.send("Ürün güncellendi");
+
+});
+
+app.listen(process.env.PORT || 3000, () => {
+
+    console.log("Server çalışıyor");
+
+});
+
+// TELEGRAM BOT EKLE
+
+const TelegramBot = require("node-telegram-bot-api");
+
+const token = "8741427596:AAGokUoGuYMbFW-6IVFvQYWx7I8ktdWu8xw";
+
+const bot = new TelegramBot(token, {
+    polling: true
+});
+
+console.log("Bot çalışıyor...");
+
+bot.onText(/\/urun (.+)/, (msg, match) => {
+
+    const chatId = msg.chat.id;
+
+    try {
+
+        const data = match[1].split("|");
+
+        const product = {
+
+            name: data[0],
+            price: data[1],
+            image: data[2]
+
+        };
+
+        fs.writeFileSync(
+            "product.json",
+            JSON.stringify(product, null, 2)
+        );
+
+        bot.sendMessage(
+            chatId,
+            "✅ Ürün güncellendi!"
+        );
+
+    } catch (err) {
+
+        bot.sendMessage(
+            chatId,
+            "❌ Format hatalı!\n/urun Ürün|Fiyat|ResimURL"
+        );
+
+    }
+
+});
